cf391d82623724ad18cfa257ca5134daa300b4b5
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2024-02-22 09:30:06 +0530

    adding service layer functions for core logic management

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,22 +1,51 @@
-import { Component, Inject, inject } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ActivatedRoute } from "@angular/router";
+import { HousingService } from "../housing.service";
+import { HousingLocation } from "../housing-location";
 @Component({
   selector: "app-details",
   standalone: true,
   imports: [CommonModule],
   template: `
-    <p>details works!</p>
-    <section>
-      {{ hosuingLocationId }}
-    </section>
+    <article>
+      <!-- "?" is used to guard gainst undefined. If id property is undefined it will not be accessed -->
+      <img
+        [src]="housingLocation?.photo"
+        [alt]="housingLocation?.name"
+        class="listing-photo"
+      />
+      <section class="listing-description">
+        <h2 class="listing-heading">{{ housingLocation?.name }}</h2>
+        <p class="listing-location">
+          {{ housingLocation?.city }}, {{ housingLocation?.state }}
+        </p>
+      </section>
+      <section class="listing-features">
+        <h2 class="section-heading">About this Location</h2>
+        <ul>
+          <li>Units Available: {{ housingLocation?.availableUnits }}</li>
+          <li>Does this location have wifi: {{ housingLocation?.wifi }}</li>
+          <li>
+            Does this location have laundry: {{ housingLocation?.laundry }}
+          </li>
+        </ul>
+      </section>
+      <section class="listing-apply">
+        <h2 class="section-heading">Apply Now to live here</h2>
+        <button class="primary" type="button">Apply Now</button>
+      </section>
+    </article>
   `,
   styleUrls: ["./details.component.css"],
 })
 export class DetailsComponent {
   route: ActivatedRoute = inject(ActivatedRoute);
-  hosuingLocationId: number = 0;
+  housingLocation: HousingLocation | undefined;
+  housingService: HousingService = inject(HousingService);
   constructor() {
-    this.hosuingLocationId = Number(this.route.snapshot.params["id"]);
+    const hosuingLocationId = Number(this.route.snapshot.params["id"]);
+    this.housingLocation =
+      this.housingService.getHousingLocationById(hosuingLocationId);
   }
 }
