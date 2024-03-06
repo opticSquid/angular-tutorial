92ae530728d8a38dca03ded33fe1ac3fdb0d52af
Soumalya Bhattacharya <soumalyabhattacharya6@gmail.com>
2024-03-06 12:22:18 +0530

    Implementing search using template varibale

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,14 +10,25 @@ import { HousingService } from "../housing.service";
   template: `
     <section>
       <form>
-        <input placeholder="filter by city" type="text" />
-        <button class="primary" type="button">Search</button>
+        <!--
+          '#' creates a variable called "template variable"
+          this template variable gives us access to the value of inout field which we can pass to else where.
+          Note: We are not controlling the state of the input so, it is an uncontrolled component. While FormGroup creates a controlled component
+      -->
+        <input placeholder="filter by city" type="text" #filter />
+        <button
+          class="primary"
+          type="button"
+          (click)="filterResults(filter.value)"
+        >
+          Search
+        </button>
       </form>
       <section class="results">
         <!--*ngFor is a for loop and you see a forEach kinda loop-->
         <!--[<custom_propery_name] is used to pass data to that parameter as you do in react-->
         <app-housing-location
-          *ngFor="let housingLocation of housingLocationList"
+          *ngFor="let housingLocation of filteredLocationList"
           [housingLocation]="housingLocation"
         />
       </section>
@@ -28,11 +39,23 @@ import { HousingService } from "../housing.service";
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
   constructor() {
-    this.housingService.getAllHousingLocations().then((housingLocationList:HousingLocation[])=>{
-      this.housingLocationList = housingLocationList;
-    }).catch(error=>{
-      console.error("Error in getting list of housing locations");
-  });
+    this.housingService
+      .getAllHousingLocations()
+      .then((housingLocationList: HousingLocation[]) => {
+        this.housingLocationList = housingLocationList;
+        this.filteredLocationList = housingLocationList;
+      })
+      .catch((error) => {
+        console.error("Error in getting list of housing locations");
+      });
+  }
+  filterResults(text: String) {
+    this.filteredLocationList = !text
+      ? this.housingLocationList
+      : this.housingLocationList.filter((housingLocation: HousingLocation) =>
+          housingLocation?.city.toLowerCase().includes(text.toLowerCase())
+        );
   }
 }
